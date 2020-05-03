9ad0fc1d7136e1ee26f6d89bd5a99428735a0570
dejda <jasiuczapiewski99@gmail,com>
2020-05-03 14:11:10 +0200

    Zad 3/4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -3,5 +3,20 @@ package com.company;
 public class Human {
     Animal pet;
     Car car;
+    Double salary;
+
+    public void setSalary(Double salary) {
+        if(salary <= 0){
+            System.out.println("U are poor");
+        }
+        System.out.println("your salary sent to the accountant ");
+
+
+
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Animal dog = new Animal("labrador", 20.0);
         Human Czapiewski = new Human();
         Czapiewski.car = new Car("720s","McLaren");
-
+        Czapiewski.setSalary(214.21);
 
     }
 }
